933550269539c54452f606800973195246f7d01b
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-05 12:43:32 -0700

    last digit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for a in range(ord('z'), ord('A') -1, -1):
+for a in range(ord('z'), ord('A') - 1):
     c = chr(a)
     case = 'lower' if a % 2 == 0 else 'upper'
     print("{}".format(c), end='') if case == 'lower' else print("{}".format(c.upper()), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(abs(number) % 10), end='')
+    r = abs(number) % 10
+    print("{}".format(r), end='')
+    return r
