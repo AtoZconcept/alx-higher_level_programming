f4467c33828258e041736ed9a5220e02182883a5
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-09 09:00:37 -0700

    tupple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not add_tuple:
-        return
-    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
-    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
-    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
-    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
-    answer = ("({:d}, {:d})".format(a1 + b1, a2 + b2))
-    return answer
+    l1 = len(tuple_a)
+    l2 = len(tuple_b)
+
+    if l1 > 1:
+        n1, n2, *lest = tuple_a
+    if l2 > 1:
+        nm1, nm2, *lest = tuple_b
+    if l1 == 1:
+        n1 = tuple_a[0]
+        n2 = 0
+    if l2 == 1:
+        nm1 = tuple_b[0]
+        nm2 = 0
+    if l1 == 0:
+        n1 = 0
+    if l1 == 0:
+        n1 = 0
+        n2 = 0
+    if l2 == 0:
+        nm1 = 0
+        nm2 = 0
+    new = (n1 + nm1, n2 + nm2)
+    return new
