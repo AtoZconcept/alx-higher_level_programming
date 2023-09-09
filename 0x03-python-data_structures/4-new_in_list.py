c21bcc2c500a6e1f40bf1e33939bfa9f08462470
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-09 02:34:28 -0700

    new_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
-    new_element = element
     if idx < 0:
         return my_list.copy()
     if idx > len(my_list):
         return my_list.copy()
-    new_list = list(my_list)
-    new_list.pop(idx)
-    new_list.insert(idx, element)
-    return "{}".format(new_list)
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
