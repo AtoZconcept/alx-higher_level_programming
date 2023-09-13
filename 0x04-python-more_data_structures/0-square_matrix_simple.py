4b883ef31b8422b6fa53eed6a5901f34dd0b74c4
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-13 08:56:09 -0700

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/puthon3
 
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+    if not matrix:
+        return matrix
+    new = []
+    for row in matrix:
+        new.append(list(map(lambda n: n**2, row)))
+    return new
