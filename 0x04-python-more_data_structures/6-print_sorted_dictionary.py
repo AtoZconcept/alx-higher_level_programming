c3817df34612d452be1f9ecc6fa8db6edf649297
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-13 08:33:27 -0700

    sorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary.keys()):
+        value = a_dictionary[i]
+        print("{}: {}".format(i, value))
