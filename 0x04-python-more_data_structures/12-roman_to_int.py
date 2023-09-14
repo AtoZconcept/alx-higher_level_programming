8d0e67668175dde1ec580a072c66804a610aab3a
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-13 20:31:32 -0700

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,13 +12,13 @@ def roman_to_int(roman_string):
             'C' : 100,
             'D' : 500,
             'M' : 1000
-            }
+    }
 
     result = 0
     prev_value = 0
 
-    for char in roman_string[::-1]:
-        value = roman_value[char]
+    for char in reversed(roman_string):
+        value = roman_value.get(char, 0)
         if value < prev_value:
             result -= value
         else:
