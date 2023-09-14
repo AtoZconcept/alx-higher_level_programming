8a280fab11fa8881fa3f2ad4273e3c13111885f5
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-14 00:22:22 -0700

    read

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,47 +1,30 @@
-#include <stddef.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * is_palindrome - Checks if a linkedList is a palindrome
- * @head: Double pointer to the head of a list
- * Return: 0 if not else 1
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
+ * Return: 1 if the list is a palindrome, 0 otherwise
  */
-
 int is_palindrome(listint_t **head)
 {
-	int len = 0, first = 0, last = 0;
-	int middle = 0, arr_l[25] = {0}, t_len;
-	listint_t *ptr;
+	listint_t *current = *head;
+	int values[2048], i = 0, j;
 
-	(void)last;
-	(void)t_len;
-
-	if (head == NULL || *head == NULL)
-		return (1);
-	/*Find the length*/
-	first = (*head)->n;
-	ptr = *head;
-
-	while (ptr != NULL)
-	{
-		len++;
-		if (len == 4)
-			middle = ptr->n;
-		if (ptr->next == NULL)
-			last = ptr->n;
-		ptr = ptr->next;
-	}
-	ptr = *head, t_len = len;
-	while (ptr != NULL)
-	{
-		arr_l[len] = ptr->n;
-		len--;
-		ptr = ptr->next;
-	}
-
-	if (first == arr_l[1] && last == arr_l[t_len] && middle == arr_l[4])
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	return (0);
+	while (current)
+	{
+		values[i] = current->n;
+		current = current->next;
+		i++;
+	}
+
+	for (j = 0; j < i / 2; j++)
+	{
+		if (values[j] != values[i - j - 1])
+			return (0);
+	}
+
+	return (1);
 }
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ALX Higher level Programming projects
 
+
+# Course Outline:
+
+
 ## Python Hello World
 
 ## 0x01. Python - if/else, loops, functions
@@ -7,3 +11,5 @@
 ## 0x02-python-import_modules
 
 ## 0x03-python-data_structures
+
+## 0x04. Python - More Data Structures: Set, Dictionary
