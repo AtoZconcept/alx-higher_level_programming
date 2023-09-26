f5f71427e5f86a134e737bbd3e2362e30511b870
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-26 12:21:04 -0700

    line

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Imagine class that is empty
+"""Imagine class that is empty.
 
-This is a docstring the 0-square
+This is a docstring the 0-square.
 
 """
 
-class Square:
-    """This is Square class
 
-    This code is creating empty Square class
+class Square:
+    """This is an empty class.
+
+    This code is creating empty Square class.
 
     """
 
