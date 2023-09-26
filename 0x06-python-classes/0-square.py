0343605e4e379b63aa07cf40d7ab05a3ea2f91e9
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-26 11:53:30 -0700

    comment

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 class Square:
     """My square function
 
     This code is creating empty square and there's no arguments
 
     Returns: It return an empty square
+
     """
     pass
