ae17f526e3bfb2c9814b3d5b79cebdcb7adeb2bf
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-26 14:51:45 -0700

    update

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This is Square with private size
+"""
+
+
+class Square:
+    """
+    This is class Square
+    """
+
+    def __init__(self, size=0):
+        """
+        This initialising of self size to 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Property to return
+
+        Returns:
+        Selve value to return
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Fuction to set size setter
+        """
+
+        if not isinstance(value, int):
+            """
+            If the size not integer
+            """
+
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """
+            If size is less than zero
+            """
+
+            raise ValueError("size must be >= 0")
+        else:
+            """
+            This is passing private size
+            """
+
+            self.__size = value
+
+    def area(self):
+        """
+        This calculate the area
+        """
+
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        """
+        This handle the print
+        """
+
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
