1b492c9cb49f9e2ffddd5d6094ed840316553dd4
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-26 12:38:41 -0700

    ohay

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is square with size
+"""
+
+
+class Square:
+    """
+    This square class about to recieve attribute
+    """
+
+    def __init__(self, size):
+        """
+        This is innitianizing self with size
+        """
+
+        self.__size = size
