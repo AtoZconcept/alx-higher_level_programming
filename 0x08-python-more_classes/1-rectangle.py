5e35e8343d2fa606dbe76a1c5aec80d450a96e03
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-02 15:00:54 -0700

    add

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = value
+            self.__width = width
 
     @property
     def height(self):
@@ -43,4 +43,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = value
+            self.__height = height
