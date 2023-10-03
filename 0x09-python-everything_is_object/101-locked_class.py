c1cb8af00958953c13868bbe037667d5d3e0806e
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-03 13:02:15 -0700

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" Function for locked key """
+
 
 class LockedClass:
+    """ Class Locked to create lock """
+
     def __setattr__(self, attr, value):
         if attr == 'first_name':
             self.__dict__[attr] = value
