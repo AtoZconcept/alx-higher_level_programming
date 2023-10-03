b84070b0b0b640027d1e1930a2b33e732575213c
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-03 02:15:43 -0700

    pycode

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col):
-    # Check if a queen can be placed at board[row][col] without attacking others
+    # Check if a queen can be placed at board[row][col]
     N = len(board)
 
     # Check the row on the left side
@@ -22,6 +23,7 @@ def is_safe(board, row, col):
 
     return True
 
+
 def solve_nqueens(board, col):
     N = len(board)
 
@@ -38,6 +40,7 @@ def solve_nqueens(board, col):
 
     return res
 
+
 def print_solution(board):
     N = len(board)
     solution = []
@@ -52,6 +55,7 @@ def print_solution(board):
     print("\n".join(solution))
     print()
 
+
 def nqueens(N):
     if not N.isdigit():
         print("N must be a number")
@@ -65,6 +69,7 @@ def nqueens(N):
     board = [[0 for _ in range(N)] for _ in range(N)]
     if not solve_nqueens(board, 0):
         print("No solution found")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
