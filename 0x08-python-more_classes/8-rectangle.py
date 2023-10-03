bc70a1fb91d34c54476dcb19b4fbe6bd8f73924b
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-03 01:52:17 -0700

    complete

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,11 +5,15 @@
 class Rectangle:
     """ class of rectangle """
 
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """ Innitializing my rectangle """
 
         self.height = height
         self.width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -65,8 +69,24 @@ class Rectangle:
             return ""
         rectangle_str = ""
         for _ in range(self.__height):
-            rectangle_str += "#" * self.__width + "\n"
+            rectangle_str += str(self.print_symbol) * self.__width + "\n"
         return rectangle_str[:-1]
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
