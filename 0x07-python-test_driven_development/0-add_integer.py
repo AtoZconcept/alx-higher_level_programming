9cbf44c45c229c2a14bd476f7dd37932ac5fc9c0
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-05 01:00:39 -0700

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" Fucntion that add integers """
+
 
 def add_integer(a, b=98):
+    """ define add integers """
+
     if not isinstance(a, (int, float)):
         return "a must be an integer"
     if not isinstance(b, (int, float)):
