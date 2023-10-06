6003107fb197200f0fb828fa35931473e059cdd7
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-06 15:48:02 -0700

    add

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,5 +16,17 @@ class TestMaxInteger(unittest.TestCase):
         """ max in the middle """
         self.assertEqual(max_integer([2, 4, 7, 6, 5]), 7)
 
+        """ negative in middle """
+        self.assertEqual(max_integer([2, -4, 7, 6, 5]), 7)
+
+        """ only negative """
+        self.assertEqual(max_integer([-2, -4, -7, -6, -5]), -2)
+
+        """ one element """
+        self.assertEqual(max_integer([7]), 7)
+
+        """ list empty """
+        self.assertIsNone(max_integer([]))
+
 if __name__ == '__main__':
     unittest.main()
