474c9b6eb8dae04dbc1e9b589b00daa6b984fdc3
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-09 09:28:23 -0700

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" this code return same class or inheritance """
+
+def is_kind_of_class(obj, a_class):
+    """ define class to return class that's same """
+
+    return isinstance(obj, a_class)
