6df9eb8a4246dac34365ddefee7cd4fd0ee6b2ae
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-09 15:38:36 -0700

    add

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,21 +2,7 @@
 """ rectange that inherit rom geometry """
 
 
-class BaseGeometry:
-    """ class for geometry innitialization """
-
-    def area(self):
-        """ defining area of geometry """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ this validate the value """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -25,10 +11,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ initializing rectangle """
 
-        self.__width = 0
-        self.__height = 0
-
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
