7c798126db4d5754f25e531ce1b41cc1c6e66e3e
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-09 09:47:48 -0700

    add

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function for improved geometry """
+
+
+class BaseGeometry:
+    """ class for geometry innitialization """
+
+    def area(self):
+        """ defining area of geometry """
+
+        raise Exception("area() is not implemented")
