5e7bc141aaecbcb075fc737b1e664d023abda318
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-09 06:37:57 -0700

    add

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,4 +5,55 @@ class Node:
         self.data = data
         self.next_node = next_node
 
+    @property
+    def data(self):
+        return self.__data
 
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if not value is None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        if value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        result = []
+        current = self.head
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+        return "\n".join(result)
