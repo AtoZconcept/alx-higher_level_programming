0aa9bd8ae674d2c84fa7471bded6867da60e5a5f
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-10 11:02:43 -0700

    add

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" student to json """
+
+
+class Student:
+    """ this is class of student """
+
+    def __init__(self, first_name, last_name, age):
+        """ defining the public inztances"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ list, dictionary, string, integer and boolean) for JSON """
+        
+        if attrs is None:
+            return self.__dict__
+
+        serial = {}
+
+        for attr in attrs:
+            if hasattr(self, attr):
+                serial[attr] = getattr(self, attr)
+
+        return serial
