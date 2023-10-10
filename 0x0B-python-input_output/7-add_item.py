a2a0fdc721e01f7971694900a20fab5c1fe07bd2
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-10 07:30:09 -0700

    add

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Script that adds all arguments to a Python list and save them a file """
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_argument(args):
+    """ function to add argument to the list """
+
+    try:
+        existing_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        existing_list = []
+
+    existing_list.extend(args)
+
+    save_to_json_file(existing_list, "add_item.json")
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+
+    if arguments:
+        add_argument(arguments)
