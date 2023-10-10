02efb2fc2c8efc46835cd9bb62bcbfd4fbe37f94
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-10-10 06:04:46 -0700

    add

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that appends a string at the end of a text file """
+
+
+def append_write(filename="", text=""):
+    """ this append the text """
+
+    with open(filename, mode='a+', encoding='utf-8') as file:
+        return file.write(text)
