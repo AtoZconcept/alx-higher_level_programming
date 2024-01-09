d4ec97de52235ec5368038143ba7af8d49c2afa6
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-09 10:37:05 -0800

    add

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Squar = require('./5-square');
+
+class Square extends Squar {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
