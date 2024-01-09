e407a6d5a5299e89664f35f9e57307a1d3d5ce44
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-09 08:44:19 -0800

    add

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {};
+      Object.create(null);
     } else {
       this.width = w;
       this.height = h;
