add1dfcfb42171d43d9cdeb89b8c32c5febf16a3
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-13 09:02:31 -0800

    add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,20 +7,22 @@ import sys
 
 
 def list_states(username, password, database):
-	"""defining to connect to database"""
-	db = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=username,
-		passwd=password,
-		db=database
-	)
+    """defining to connect to database"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
 
-	cursor = db.cursor()
-	cursor.execute("SELECT * FROM states ORDER BY id")
-	rows = cursor.fetchall()
-	for row in rows:
-		print(row)
-if __name__ ==  "__main__":
-	username, password, database = sys.argv[1:]
-	list_states(username, password, database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1:]
+    list_states(username, password, database)
