735cd2fb5c8a4001647839e9b04eeffc13a16fda
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-13 09:10:47 -0800

    add

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database):
+    """defining to connect to database"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1:]
+    list_states(username, password, database)
