0293cb55c0b8a8d47f5566fbe89b143d9499e747
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-29 06:49:26 -0800

    add

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,9 +10,10 @@ from urllib.error import URLError
 if __name__ == "__main__":
     req = Request(sys.argv[1])
     try:
-        response = urlopen(req)
+        with urlopen(req) as response:
+            body = response.read().decode('utf-8')
     except URLError as e:
         if hasattr(e, 'code'):
             print('Error code:', e.code)
     else:
-        print("Index")
+        print(body)
