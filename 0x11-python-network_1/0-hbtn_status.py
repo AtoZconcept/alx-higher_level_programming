5d08cc095112905457c85e755a8afc86a516b4da
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-29 05:14:34 -0800

    task-0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+
+        print('Body response:')
+        print('\t- type:', type(res)) 
+        print("\t- content:", res)
+        print('\t- utf8 content:', res.decode('utf-8'))
