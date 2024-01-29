cdec49a3e5302d6c57460be8d845cbe4e29c2fb6
AtoZconcept <opeyemiolaafeez.com>
2024-01-29 17:56:51 +0100

    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,13 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     commits = requests.get(url).json()
-    for i in range(10):
-        print(
-                "{}: {}".format(
-                    commits[i].get("sha"),
-                    commits[i].get("commit").get("author").get("name"),
+    try:
+        for i in range(10):
+            print(
+                    "{}: {}".format(
+                        commits[i].get("sha"),
+                        commits[i].get("commit").get("author").get("name"),
+                        )
                     )
-                )
+    except IndexError:
+        pass
