aed2bdd0ac0e5adbf994eb5e150715768711e86d
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-29 07:01:52 -0800

    add

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+import requests
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url, auth=('user', 'pass'))
+    print("Body response:")
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
