257b6e47ae04af0cf383480867521200aed83173
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-29 08:48:47 -0800

    add

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that print all commit message and autho name"""
+
+
+import requests
+from sys import argv
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    commits = requests.get(url).json()
+    for i in range(10):
+        print(
+                "{}: {}".format(
+                    commits[i].get("sha"),
+                    commits[i].get("commit").get("author").get("name"),
+                    )
+                )
