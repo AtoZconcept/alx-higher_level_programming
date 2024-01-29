d37c4d0f1e01f84000608601d71250aea19284d0
AtoZconcept <opeyemiolaafeez@gmail.com>
2024-01-29 06:13:59 -0800

    task-2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)
+"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('utf-8')
+    with urllib.request.urlopen(sys.argv[1], data=data) as response:
+        body = response.read().decode('utf-8')
+        print('Your email is:', sys.argv[2])
