525383b4c17232c3a510f201521ec79ffff9e80c
AtoZconcept <opeyemiolaafeez.com>
2024-02-26 10:44:57 +0100

    add

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const wedgeAntillesFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+    console.log(wedgeAntillesFilms.length);
+  }
+});
