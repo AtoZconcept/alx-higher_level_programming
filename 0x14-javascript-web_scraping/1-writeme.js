72d48a8c50e973a4b2393fffc2803f04a2d82e1b
AtoZconcept <opeyemiolaafeez.com>
2024-02-26 09:17:30 +0100

    task 1 add

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function writeToFile ()
+function writeToFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+writeToFile(filePath, content);
