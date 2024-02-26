35d2fa2421a315ead350815a594895def3e78b3e
AtoZconcept <opeyemiolaafeez.com>
2024-02-26 09:06:21 +0100

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readAndPrint(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
+function readAndPrint (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
 if (process.argv.length < 3) {
-    console.error('Usage: node scipty.js <file>');
-    process.exit(1);
+  console.error('Usage: node scipty.js <file>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
