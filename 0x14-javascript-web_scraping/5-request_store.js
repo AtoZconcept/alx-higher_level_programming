b17f0459fab6c3f91cf8eeaf5c83f11199f3deff
AtoZconcept <opeyemiolaafeez.com>
2024-02-26 11:00:05 +0100

    add

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+function writeToFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+	  if (err) {
+      console.log(err);
+	  }
+  });
+}
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filePath = process.argv[3];
+    writeToFile(filePath, body);
+  }
+});
